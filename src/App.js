733e45c57e5b3ba7ddb98122f1f2add71b64e31b
Camila Wolter <camila0wolter@gmail.com>
2022-07-25 17:00:10 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,9 @@ class App extends React.Component {
         <div>
           <h1>Tryunfo!</h1>
           <Form />
+        </div>
+        <div>
+          <Card />
         </div>
       </main>
     );
